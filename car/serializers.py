ff80b1db0c5ef91aa21b9bfb6b765426aa89fc75
kavbian <kavbian@gmail.com>
2025-04-26 14:59:52 +0200

    fix_serializer

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from car.models import Car
+
 
 class CarSerializer(serializers.Serializer):
     manufacturer = serializers.CharField(max_length=64)
@@ -7,3 +9,15 @@ class CarSerializer(serializers.Serializer):
     horse_powers = serializers.IntegerField()
     is_broken = serializers.BooleanField()
     problem_description = serializers.CharField(allow_blank=True)
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.manufacturer = validated_data.get('manufacturer', instance.manufacturer)
+        instance.model = validated_data.get('model', instance.model)
+        instance.horse_power = validated_data.get('horse_powers', instance.horse_powers)
+        instance.is_broken = validated_data.get('is_broken', instance.is_broken)
+        instance.problem_description = validated_data.get('problem_description', instance.problem_description)
+        instance.save()
+        return instance
