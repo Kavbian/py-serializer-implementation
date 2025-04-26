e14bade8bdff07eac11d21d4e01c33af163936b6
kavbian <kavbian@gmail.com>
2025-04-26 15:10:54 +0200

    serializer_fix

--- car/serializers.py
+++ car/serializers.py
@@ -7,9 +7,9 @@ class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
-    horse_powers = serializers.IntegerField(min_value=1, max_value=5000)
+    horse_powers = serializers.IntegerField(min_value=1, max_value=1000)
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(allow_null=True)
+    problem_description = serializers.CharField(required=False)
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
