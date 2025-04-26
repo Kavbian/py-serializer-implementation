6e7f232a82a5bb1fa5bf98357d68907fb192288d
kavbian <kavbian@gmail.com>
2025-04-26 15:08:33 +0200

    flake8

--- car/serializers.py
+++ car/serializers.py
@@ -7,9 +7,9 @@ class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64)
     model = serializers.CharField(max_length=64)
-    horse_powers = serializers.IntegerField()
+    horse_powers = serializers.IntegerField(min_value=1, max_value=5000)
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(allow_blank=True)
+    problem_description = serializers.CharField(allow_null=True)
 
     def create(self, validated_data):
         return Car.objects.create(**validated_data)
